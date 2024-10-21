59b5b64712f2746bee6b60f0560fd3be62f55336
Magaly20 <63077525+Magaly20@users.noreply.github.com>
2024-10-20 21:29:16 -0600

    Add files via upload

---
+++ R2MEB_MRV.R
@@ -0,0 +1,7 @@
+#Reporte_2
+getwd()
+setwd("/Users/magy_rivera/Documents/Maestria_FCFM/1er_Tetra/Estadistica/Tareas/Reportes_MEB/R1MEB_MRV")
+mi_data <- read.csv("Suicide_Data.csv", header = TRUE, sep = ",")
+head(mi_data)
+#Ver características de cada variable
+summary(data$suicides.100k.pop)
